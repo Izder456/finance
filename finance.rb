c1140bc0671a5449c8a1ad6c297b115735cff3de
izder456 <izder456@protonmail.com>
2020-09-27 16:59:02 -0500

    update ruby source to be more concise

--- finance.rb
+++ finance.rb
@@ -5,25 +5,24 @@ Version : v1.0
 License : N/A
 =end
 
-def calculate(p,r,t,n)
-  power = (1+(r / n))**(t * n)
-  roi = p * power
-  return roi
+def input()
+	print "Principal: "
+	p = gets.chomp.to_f
+	print "Rate : "
+	r = gets.chomp.to_f
+	print "Length (in Years) : "
+	t = gets.chomp.to_f
+	print "Times per year : "
+	n = gets.chomp.to_i
+	return p,r,t,n
 end
 
-print "Principal: "
-p = gets.chomp.to_f
-print "Rate : "
-r = gets.chomp.to_f
-print "Length (in Years) : "
-t = gets.chomp.to_f
-print "Times per year : "
-n = gets.chomp.to_i
-principal = p
-rate = r
-year = t
-times = n
-print "Interest : "
-print calculate(principal, rate, year, times)
+def calculate(p,r,t,n)
+	return ((p) * (1 + (r / n)))**((n) * (t))
+end
+
+
+p,r,t,n = input()
+print calculate(p,r,t,n)
 
 
